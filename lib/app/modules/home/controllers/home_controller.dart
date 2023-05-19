81e15b2a511185952c27075e94295251c14a5767
josercc <josercc@163.com>
2023-05-19 17:48:06 +0800

    完成了打开 Flutter 项目自动分析 项目依赖库列表

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,34 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
+
+import 'package:flutter_runtime_ide/app/data/package_config.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
-  late String _progectPath;
+  // 当前操作的工程路径
+  var progectPath = "".obs;
+
+  /// 当前工程的第三方库的配置
+  var packageConfig = Rx<PackageConfig?>(null);
+
   HomeController() {
-    _progectPath = Get.arguments as String;
+    progectPath.value = Get.arguments as String;
+    readPackageConfig();
+  }
+
+  // 读取当前工程的第三方库的配置
+  Future<void> readPackageConfig() async {
+    String packageConfigPath =
+        join(progectPath.value, ".dart_tool", "package_config.json");
+    // 判断文件是否存在
+    if (!await File(packageConfigPath).exists()) {
+      Get.snackbar("错误!", "请先执行 flutter pub get");
+      return;
+    }
+
+    // 读取文件内容
+    String content = await File(packageConfigPath).readAsString();
+    packageConfig.value = PackageConfig.fromJson(jsonDecode(content));
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_runtime_ide/app/data/package_config.dart';
 
 import 'package:get/get.dart';
 
@@ -12,10 +13,89 @@ class HomeView extends GetView<HomeController> {
       appBar: AppBar(
         title: const Text('首页'),
         centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.analytics))
+        ],
       ),
-      body: Row(
-        children: [],
+      body: Obx(
+        () => Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              color: Colors.blue.shade100,
+              child: Column(
+                children: [
+                  _titleValueWidget("工程路径", controller.progectPath.value),
+                  if (controller.packageConfig.value == null)
+                    Container()
+                  else
+                    Column(
+                      children: [
+                        _titleValueWidget(
+                          "configVersion",
+                          controller.packageConfig.value!.configVersion
+                              .toString(),
+                        ),
+                        _titleValueWidget(
+                          "generated",
+                          controller.packageConfig.value!.generated,
+                        ),
+                        _titleValueWidget(
+                          "generator",
+                          controller.packageConfig.value!.generator,
+                        ),
+                        _titleValueWidget(
+                          "generatorVersion",
+                          controller.packageConfig.value!.generatorVersion,
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  PackageInfo packageInfo =
+                      controller.packageConfig.value!.packages[index];
+                  return Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        _titleValueWidget("name", packageInfo.name),
+                        _titleValueWidget("rootUri", packageInfo.rootUri),
+                        _titleValueWidget("packageUri", packageInfo.packageUri),
+                        _titleValueWidget(
+                            "languageVersion", packageInfo.languageVersion),
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => const Divider(),
+                itemCount: controller.packageConfig.value?.packages.length ?? 0,
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _titleValueWidget(String title, String value) {
+    return Row(children: [
+      SizedBox(
+        width: 150,
+        child: Text(
+          "$title:",
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      Expanded(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(value, style: TextStyle(color: Colors.grey.shade600)),
+        ),
+      ),
+    ]);
   }
 }
